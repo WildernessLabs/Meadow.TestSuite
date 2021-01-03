345ce64da10cecfff3cadd1fb4185323ac582a73
ctacke <ctacke@opennetcf.com>
2021-01-03 12:41:45 -0600

    Make test more automated

--- Test Assemblies/Tests.Mcp23x08/InterruptTests.cs
+++ Test Assemblies/Tests.Mcp23x08/InterruptTests.cs
@@ -43,6 +43,8 @@ namespace Peripheral
             Assert.NotNull(bus);
 
             var meadow_interrupt_count = 0;
+            var expander_event_count = 0;
+            var input_event_count = 0;
 
             // this is the meadow interrupt that will forward expander interrupts
             using (var int_port = Device.CreateDigitalInputPort(
@@ -64,7 +66,9 @@ namespace Peripheral
                 Assert.NotNull(expander, "Failed to create the Expander object");
                 expander.InputChanged += (sender, args) =>
                 {
-                    Output.WriteLineIf(ShowDebug, $"! Expander Input Changed state: {Convert.ToString(args.InputState, 2)}");
+                    expander_event_count++;
+                    // Output.WriteLineIf(ShowDebug, $"! Expander Input Changed state: {Convert.ToString(args.InputState, 2)}");
+                    Output.WriteLineIf(ShowDebug, $"Expander Input Changed");
                 };
 
                 // BUG BUG BUG - we don't pass this currently
@@ -74,21 +78,48 @@ namespace Peripheral
                 var gp_input = expander.CreateDigitalInputPort(expander.Pins.GP7, InterruptMode.EdgeBoth, ResistorMode.PullUp);
                 gp_input.Changed += (sender, args) =>
                 {
+                    input_event_count++;
                     // dev note
                     // BUG BUG BUG
                     // Without the gp_input.State read here, this event will never fire a second time
-                    Output.WriteLineIf(ShowDebug, $"Expanded input interrupt. Event says it's {args.Value}. Pin says it's {gp_input.State}.");
+                    Output.WriteLineIf(ShowDebug, "Expanded input interrupt");
+                    // Output.WriteLineIf(ShowDebug, $"Expanded input interrupt. Event says it's {args.Value}. Pin says it's {gp_input.State}.");
                 };
+
+                // create an output to drive the input
+                var gp_output = expander.CreateDigitalOutputPort(expander.Pins.GP6, true);
 
                 // BUG BUG BUG - we don't pass this currently
                 Assert.Equal(0, meadow_interrupt_count, "We got an unsolicted interrupt from the MCP Input Creation");
 
+                var tests_to_perform = 3;
+
+                for(int i = 0; i < tests_to_perform; i++)
+                {
+                    Output.WriteLineIf(ShowDebug, $"Test {i+1}...");
+                    // generate some interrupts
+
+                    // drive low
+                    gp_output.State = false;
+                    // wait
+                    Thread.Sleep(250);
+                    // drive high
+                    gp_output.State = true;
+                    // wait
+                    Thread.Sleep(250);
+                }
+
+                // we should have 2 interrupts per iteration (rising and falling)
+                // TODO: could them separately
+                Assert.Equal(tests_to_perform * 2, input_event_count, "Incorrect event count");
+/*
                 // TODO: add loopbacks - for now it's a manual test.  Just pull GP low and then back high.
                 for (int i = 0; i < 30; i++)
                 {
                     Output.WriteLineIf(ShowDebug, $"tick");
                     Thread.Sleep(1000);
                 }
+*/
             }
         }
     }
