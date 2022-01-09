319718087a31c5ad57378bfd8d8169f7e78ed5bc
Chris Tacke <ctacke@opennetcf.com>
2022-01-09 11:39:18 -0600

    Working on TestDirector

--- TestExplorer/ViewModels/MainWindowViewModel.cs
+++ TestExplorer/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
 using Avalonia.Controls;
+using Meadow.TestSuite;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace TestExplorer.ViewModels
@@ -12,6 +15,40 @@ namespace TestExplorer.ViewModels
     {
         private string? _assemblyPath;
         private string[]? _assemblies;
+        private string? _connectionStatus = "Not tested.";
+        private string? _meadowAddress;
+        private RestTestDirector? _director;
+
+        public ObservableCollection<string> AssembliesToSend { get; } = new ObservableCollection<string>();
+
+        public MainWindowViewModel()
+        {
+            AssembliesToSend.CollectionChanged += (s, e) =>
+            {
+                this.RaisePropertyChanged(nameof(SendEnabled));
+            };
+        }
+
+        public string? MeadowAddress
+        {
+            get => _meadowAddress;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _meadowAddress, value);
+                this.RaisePropertyChanged(nameof(AddressIsReasonable));
+            }
+        }
+
+        public bool AddressIsReasonable
+        {
+            get => IPEndPoint.TryParse(MeadowAddress ?? String.Empty, out IPEndPoint _);
+        }
+
+        public string? ConnectionStatus
+        {
+            get => _connectionStatus;
+            set => this.RaiseAndSetIfChanged(ref _connectionStatus, value);
+        }
 
         public string? LocalAssemblyPath
         {
@@ -19,14 +56,18 @@ namespace TestExplorer.ViewModels
             set
             {
                 if (value == LocalAssemblyPath) return;
+                _assemblyPath = value;
                 this.RaisePropertyChanged();
 
                 // get assemblies
-                var di = new DirectoryInfo(value);
-                string[] files = null;
-                if (di.Exists)
+                string[]? files = null;
+                if (LocalAssemblyPath != null)
                 {
-                    files = di.GetFiles("*.dll").Select(fi => fi.Name).ToArray();
+                    var di = new DirectoryInfo(LocalAssemblyPath);
+                    if (di.Exists)
+                    {
+                        files = di.GetFiles("*.dll").Select(fi => fi.Name).ToArray();
+                    }
                 }
 
                 Assemblies = files;
@@ -36,10 +77,41 @@ namespace TestExplorer.ViewModels
         public string[]? Assemblies
         {
             get => _assemblies;
-            set
+            set => this.RaiseAndSetIfChanged(ref _assemblies, value);
+        }
+
+        public async void OnTestConnection()
+        {
+            if (MeadowAddress == null) return;
+
+            _director = new RestTestDirector(IPEndPoint.Parse(MeadowAddress));
+
+            try
             {
-                this.RaiseAndSetIfChanged(ref _assemblies, value);
+                ConnectionStatus = $"Testing...";
+
+                var info = await _director.GetInfo();
+
+                ConnectionStatus = $"Device: {info.Name} version {info.Version}";
+
+                this.RaisePropertyChanged(nameof(SendEnabled));
+            }
+            catch (Exception ex)
+            {
+                ConnectionStatus = ex.Message;
+                _director = null;
             }
         }
+
+        public bool SendEnabled
+        {
+            get
+            {
+                if (_director == null) return false;
+                if (AssembliesToSend == null) return false;
+                return AssembliesToSend.Count > 0;
+            }
+        }
+
     }
 }
