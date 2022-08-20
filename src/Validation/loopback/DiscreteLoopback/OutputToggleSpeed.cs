4103f6eeca6860ecd427b254a0ec993f82162316
ctacke <ctacke@gmail.com>
2022-08-19 20:31:59 -0500

    Validation with JIT enabled

--- src/Validation/loopback/DiscreteLoopback/OutputToggleSpeed.cs
+++ src/Validation/loopback/DiscreteLoopback/OutputToggleSpeed.cs
@@ -9,30 +9,32 @@ namespace Validation
     {
         public bool RunTest(IMeadowDevice device, IPin pin)
         {
-            var op = device.CreateDigitalOutputPort(pin, true);
-            var count = 1000;
-
-            var stopwatch = Stopwatch.StartNew();
-
-            for(int i = 0; i < count; i++)
+            using(var op = device.CreateDigitalOutputPort(pin, true))
             {
-                op.State = false;
-                op.State = true;
+                var count = 1000;
+
+                var stopwatch = Stopwatch.StartNew();
+
+                for(int i = 0; i < count; i++)
+                {
+                    op.State = false;
+                    op.State = true;
+                }
+                stopwatch.Stop();
+                Resolver.Log.Info($"{count} pulses took {stopwatch.ElapsedMilliseconds}ms");
+
+                // NOTE 0.6.6.6 result: 1000 pulses took 120ms ( 0.86 ms/pulse )
+                var knownGood = 120d;
+
+                // look for regression of > 10%
+                var success = stopwatch.ElapsedMilliseconds < (knownGood * 1.1);
+                if(!success)
+                {
+                    Resolver.Log.Error($"Output toggle speed is out of expected range.");
+                }
+
+                return success;
             }
-
-            Resolver.Log.Info($"{count} pulses took {stopwatch.ElapsedMilliseconds}ms");
-
-            // NOTE 0.6.6.6 result: 1000 pulses took 862ms ( 0.86 ms/pulse )
-            var knownGood = 862;
-
-            // look for regression of > 10%
-            var success = stopwatch.ElapsedMilliseconds < knownGood * 1.1;
-            if(!success)
-            {
-                Resolver.Log.Error($"Output toggle speed is out of expected range.");
-            }
-
-            return success;
         }
     }
 }
