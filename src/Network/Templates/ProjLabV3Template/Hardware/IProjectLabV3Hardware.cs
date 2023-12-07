3949411ffd03fbf45e95b010c6464a9855bd7a57
Mark Stevens <mark@thepcsite.co.uk>
2023-12-07 13:01:04 +0000

    Add ProjectLab template project.

---
+++ src/Network/Templates/ProjLabV3Template/Hardware/IProjectLabV3Hardware.cs
@@ -0,0 +1,20 @@
+﻿using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Atmospheric;
+using Meadow.Peripherals.Sensors.Light;
+
+namespace ProjectLabTest.Hardware
+{
+    internal interface IProjectLabV3Hardware
+    {
+        public IGraphicsDisplay Display { get; }
+
+        public ILightSensor LightSensor { get; }
+
+        public Bme68x EnvironmentalSensor { get; }
+
+        public RgbPwmLed RgbPwmLed { get; }
+
+        public void Initialize();
+    }
+}
