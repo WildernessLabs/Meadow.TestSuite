9d452daa04d2ab32711b42b7e037a0195e27764e
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 11:45:17 +0000

    Increased the number of LEDs

--- src/Network/FeatherF7SoakTestRunner/Hardware/IOLEDBoardHardware.cs
+++ src/Network/FeatherF7SoakTestRunner/Hardware/IOLEDBoardHardware.cs
@@ -7,13 +7,7 @@ internal interface IOLEDBoardHardware
 {
     public IGraphicsDisplay Display { get; }
 
-    public Led Led1 { get; }
-
-    public Led Led2 { get; }
-
-    public Led Led3 { get; }
-
-    public Led Led4 { get; }
+    public Led[] Leds { get; }
 
     public void Initialize(F7FeatherBase device);
 }
