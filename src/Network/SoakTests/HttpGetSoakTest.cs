b37a1b7f0b39d3338b6ea58c8283556c516182fe
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 12:26:14 +0000

    Added comments.

--- src/Network/SoakTests/HttpGetSoakTest.cs
+++ src/Network/SoakTests/HttpGetSoakTest.cs
@@ -3,22 +3,36 @@ using SoakTests.Common;
 
 namespace SoakTests;
 
+/// <summary>
+/// Perform the async HttpClient-based soak test.
+/// </summary>
 class HttpGetSoakTest : ISoakTest
 {
+    /// <summary>
+    /// Soak test configuration object.
+    /// </summary>
     SoakTestSettings _config;
 
-    public string Name => "HttpGetSoakTest";
-
+    /// <summary>
+    /// Setup the test.
+    /// </summary>
+    /// <param name="config">General soak test configuration</param>
     public void Initialize(SoakTestSettings config)
     {
         _config = config;
     }
 
+    /// <summary>
+    /// Execute the test once.
+    /// </summary>
     public async void Execute()
     {
         await Helpers.GetWebPageViaHttpClient(_config.RequestUri);
     }
 
+    /// <summary>
+    /// Perform any necessary cleanup at the end of the test run.
+    /// </summary>
     public void Teardown()
     {
     }
