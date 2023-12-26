18423fd6e4a03e48ae924129d9aee96ec51100ab
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 11:52:20 +0000

    Changed toi use reflection and added comments and tests.

--- src/Network/SoakTests/RegisteredTests.cs
+++ src/Network/SoakTests/RegisteredTests.cs
@@ -3,16 +3,26 @@ namespace SoakTests;
 
 public static class RegisteredTests
 {
+    /// <summary>
+    /// Registered tests.
+    /// </summary> 
     public static ISoakTest[] Tests = new ISoakTest[]
     {
-        new HttpGetSoakTest()
+        new HttpGetSoakTest(),
+        new SocketSoakTest(),
+        new SocketAsyncSoakTest(),
     };
 
+    /// <summary>
+    /// Look for the test with the given name.
+    /// </summary>
+    /// <param name="testName">Name of the test to be executed.</param>
+    /// <returns>Pointer to the test to be executed or null if the test cannot be found.</returns>
     public static ISoakTest GetTest(string testName)
     {
         foreach (var test in Tests)
         {
-            if (test.Name == testName)
+            if (test.GetType().Name == testName)
             {
                 return test;
             }
