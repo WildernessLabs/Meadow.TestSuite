8cba08f42835f5ddd1d37e1f8057813a30d55b29
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 11:46:55 +0000

    Added some comments.

--- src/Network/FeatherF7SoakTestRunner/Hardware/IOLEDBoardHardware.cs
+++ src/Network/FeatherF7SoakTestRunner/Hardware/IOLEDBoardHardware.cs
@@ -5,9 +5,19 @@ using Meadow.Foundation.Leds;
 namespace FeatherF7Test.Hardware;
 internal interface IOLEDBoardHardware
 {
+    /// <summary>
+    /// Display used to show progress.
+    /// </summary>
     public IGraphicsDisplay Display { get; }
 
+    /// <summary>
+    /// LEDs that can be used to indicate test progress / status.
+    /// </summary>
     public Led[] Leds { get; }
 
+    /// <summary>
+    /// Initialize the hardware.
+    /// </summary>
+    /// <param name="device">Type of device the test is running on.</param>
     public void Initialize(F7FeatherBase device);
 }
