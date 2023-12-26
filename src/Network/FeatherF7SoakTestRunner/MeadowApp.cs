6d1fe9b774535f9c8d56856269144b705e63f891
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 11:44:43 +0000

    Added some error handling.

--- src/Network/FeatherF7SoakTestRunner/MeadowApp.cs
+++ src/Network/FeatherF7SoakTestRunner/MeadowApp.cs
@@ -11,33 +11,32 @@ using SoakTests.Common;
 
 namespace FeatherF7Test
 {
-    // public class MeadowApp : App<F7FeatherV1>
-    public class MeadowApp : App<F7FeatherV2>
+    public class MeadowApp : App<F7FeatherV1>
+    // public class MeadowApp : App<F7FeatherV2>
     {
         /// <summary>
-        /// 
+        /// OLED hardware object.  This object contains the OLED display and 8 LED objects.
         /// </summary>
         IOLEDBoardHardware _hardware;
 
         /// <summary>
-        /// 
+        /// SSD1306 OLED display to show progress.
         /// </summary>
         DisplayController _displayService;
 
         /// <summary>
-        /// 
+        /// Soak test configuration, this is read from the app.config.yaml file.
         /// </summary>
         SoakTestSettings _config;
 
         /// <summary>
-        /// 
+        /// Test to be run.
         /// </summary>
         ISoakTest _test;
 
         /// <summary>
-        /// 
+        /// Configure the application and run the test named in the app.config.yaml file.
         /// </summary>
-        /// <returns></returns>
         public override Task Initialize()
         {
             _config = new SoakTestSettings();
@@ -51,14 +50,27 @@ namespace FeatherF7Test
             Helpers.WaitForNetworkConnection(Device);
 
             _test = RegisteredTests.GetTest(_config.TestName);
-            _displayService.UpdateTitle(_test.Name);
+            if (_test == null)
+            {
+                _displayService.UpdateTitle("ERROR");
+                _displayService.Log($"{_config.TestName}", false);
+                _displayService.Log("not found.", false);
+                Console.WriteLine($"Test '{_config.TestName}' not found.");
+                while (true)
+                {
+                    _hardware.Leds[7].IsOn = !_hardware.Leds[7].IsOn;
+                    Thread.Sleep(500);
+                }
+                
+            }
+            _displayService.UpdateTitle(_config.TestName);
             _test.Initialize(_config);
 
             return Task.CompletedTask;
         }
 
         /// <summary>
-        /// 
+        /// Run the specified test the specified number of times.
         /// </summary>
         public override Task Run()
         {
@@ -85,11 +97,12 @@ namespace FeatherF7Test
                 {
                     Thread.Sleep(_config.DelayBetweenCyclesMs);
                 }
-                _hardware.Led1.IsOn = !_hardware.Led1.IsOn;
+                _hardware.Leds[0].IsOn = !_hardware.Leds[0].IsOn;
             }
             _test.Teardown();
 
             _displayService.Log("Done.");
+            Console.WriteLine("Done.");
 
             Thread.Sleep(Timeout.Infinite);
 
