619f8b9de4c1a14a2e2fb938ea0b152cad1d552a
Mark Stevens <mark@thepcsite.co.uk>
2023-12-26 12:19:42 +0000

    Helper class containing common methods.

--- src/Network/SoakTests/SoakTests.Common.cs
+++ src/Network/SoakTests/SoakTests.Common.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Http;
 
@@ -12,6 +13,9 @@ using Meadow.Hardware;
 
 namespace SoakTests.Common;
 
+/// <summary>
+/// 
+/// </summary>
 public static class Helpers
 {
     /// <summary>
@@ -49,6 +53,11 @@ public static class Helpers
         Console.WriteLine($"{DateTime.Now:HH:mm:ss}: {message}");
     }
 
+    /// <summary>
+    /// Wait for the network connection to be established.  This method can be used for
+    /// both wired and wireless connections. 
+    /// </summary>
+    /// <param name="device">F7 Feather base class.</param>
     public static void WaitForNetworkConnection(F7FeatherBase device)
     {
         SemaphoreSlim semaphore = new SemaphoreSlim(0, 1);
@@ -64,11 +73,22 @@ public static class Helpers
         semaphore.Wait();
         ConsoleLog("Network connection established.");
 
+        /// <summary>
+        /// Wait for a WiFi connection to be established.
+        /// </summary>
+        /// <param name="semaphore">Semaphore used to signal to the caller that the connection has been established.</param>
+        /// <param name="device">F7 Feather base class.</param>
         static void WaitForWiFiConnection(SemaphoreSlim semaphore, F7FeatherBase device)
         {
             ConsoleLog($"Connecting to router via WiFi.");
             var wifi = device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
-            if (!wifi.IsConnected)
+            if (wifi.IsConnected)
+            {
+                ConsoleLog("WiFi already connected.");
+                ConsoleLog($"IP Address: {wifi.IpAddress}");
+                semaphore.Release();
+            }
+            else
             {
                 wifi.NetworkConnected += (s, e) =>
                 {
@@ -77,14 +97,13 @@ public static class Helpers
                     semaphore.Release();
                 };
             }
-            else
-            {
-                ConsoleLog("WiFi already connected.");
-                ConsoleLog($"IP Address: {wifi.IpAddress}");
-                semaphore.Release();
-            }
         }
 
+        /// <summary>
+        /// Wait for a wired ethernet connection to be established.
+        /// </summary>
+        /// <param name="semaphore">Semaphore used to signal to the caller that the connection has been established.</param>
+        /// <param name="device">F7 Feather base class.</param>
         static void WaitForEthernetConnection(SemaphoreSlim semaphore, F7FeatherBase device)
         {
             ConsoleLog($"Connecting to router via wired ethernet.");
@@ -105,5 +124,23 @@ public static class Helpers
                 };
             }
         }
+    }
+
+    /// <summary>
+    /// Convert a URL into an endpoint.
+    /// </summary>
+    /// <param name="url">URL of interest.</param>
+    /// <returns>IPEndpoint</returns>
+    public static IPEndPoint GetEndpoint(string url)
+    {
+        var uri = new Uri(url, UriKind.Absolute);
+
+        IPAddress ipAddress = null;
+        if (!IPAddress.TryParse(uri.Host, out ipAddress))
+        {
+            var ipHostInfo = Dns.GetHostEntry(uri.DnsSafeHost);
+            ipAddress = ipHostInfo.AddressList[0];
+        }
+        return new(ipAddress, uri.Port);
     }
 }
