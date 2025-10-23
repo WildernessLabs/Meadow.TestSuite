32325b6d799e24d2ea1704f2f2901f03735363ed
Chris Tacke <ctacke@gmail.com>
2025-10-22 19:29:10 -0500

    added projlab v3 version of validation tests

---
+++ src/Validation/ProjectLab_F7v3/I2CBusTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Validation
+{
+    public class I2CBusTest<T> : ITest<T>
+        where T : ProjectLabTestDevice
+    {
+        public async Task<bool> RunTest(T device)
+        {
+            // connect to the light sensor on the bus to verify it's working
+
+            try
+            {
+                device.ProjectLab.LightSensor.StartUpdating();
+                var lux = await device.ProjectLab.LightSensor.Read();
+                Resolver.Log.Debug($"Lux: {lux}");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Error($"{this.GetType().Name} failed: {ex.Message}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Validation/ProjectLab_F7v3/MeadowApp.cs
@@ -0,0 +1,88 @@
+﻿using Meadow.Devices;
+using Meadow.Foundation.Graphics;
+using Meadow.Peripherals.Displays;
+using Meadow.Peripherals.Leds;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meadow.Validation;
+
+public class MeadowApp : ValidationApp<F7CoreComputeV2>
+{
+    private IRgbPwmLed _led;
+
+    private ProjectLabTestDevice _testDevice;
+    private MicroGraphics _graphics;
+    private bool _heartBeat;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info($"Project Lab Validation");
+
+        _testDevice = new ProjectLabTestDevice(Device, ProjectLab.Create());
+        _led = _testDevice.ProjectLab.RgbLed;
+
+        // create a display - just doing this verifies SPI
+        _graphics = new MicroGraphics(_testDevice.ProjectLab.Display);
+        _graphics.Rotation = RotationType._90Degrees;
+        _graphics.CurrentFont = new Font12x20();
+
+        return base.Initialize();
+    }
+
+    public override MeadowTestDevice DeviceUnderTest => _testDevice;
+
+    public override void DisplayTestsRunning()
+    {
+        _graphics?.DrawRectangle(0, 0, _testDevice.ProjectLab.Display.Width, _testDevice.ProjectLab.Display.Height, Color.Yellow, true);
+        _graphics.DrawText(_testDevice.ProjectLab.Display.Width / 2, _testDevice.ProjectLab.Display.Height / 2, "Running", color: Color.Black, alignmentH: HorizontalAlignment.Center, alignmentV: VerticalAlignment.Center);
+
+        _graphics.Show();
+        _led.SetColor(Color.Yellow);
+    }
+
+    public override void DisplaySuccess()
+    {
+        _graphics?.DrawRectangle(0, 0, _testDevice.ProjectLab.Display.Width, _testDevice.ProjectLab.Display.Height, Color.Lime, true);
+        _graphics.DrawText(_testDevice.ProjectLab.Display.Width / 2, _testDevice.ProjectLab.Display.Height / 2, "PASS", color: Color.Black, alignmentH: HorizontalAlignment.Center, alignmentV: VerticalAlignment.Center);
+        _graphics.Show();
+        _led.SetColor(Color.Green);
+    }
+
+    public override void DisplayFailure()
+    {
+        _graphics?.DrawRectangle(0, 0, _testDevice.ProjectLab.Display.Width, _testDevice.ProjectLab.Display.Height, Color.Red, true);
+        _graphics.DrawText(_testDevice.ProjectLab.Display.Width / 2, _testDevice.ProjectLab.Display.Height / 2, "FAIL", color: Color.White, alignmentH: HorizontalAlignment.Center, alignmentV: VerticalAlignment.Center);
+        _graphics.Show();
+        _led.SetColor(Color.Red);
+    }
+
+    public override void OnExecutionHeartbeat()
+    {
+        _heartBeat = !_heartBeat;
+
+        if (_heartBeat)
+            _led.SetColor(Color.Blue);
+        else
+            _led.SetColor(Color.Cyan);
+
+        base.OnExecutionHeartbeat();
+    }
+
+    public override IEnumerable<ITest<MeadowTestDevice>> TestsToRun
+    {
+        get
+        {
+            return new ITest<ProjectLabTestDevice>[]
+            {
+//                    new I2CBusTest<ProjectLabTestDevice>(),
+//                    new WiFiConnectionPositiveTest<ProjectLabTestDevice>(),
+//                    new SpiBusTest(),
+//                    new WiFiConnectionInvalidSsidTest<ProjectLabTestDevice>(),
+                new WiFiConnectionInvalidPasscodeTest<ProjectLabTestDevice>()
+            }
+            .Cast<ITest<MeadowTestDevice>>();
+        }
+    }
+}
